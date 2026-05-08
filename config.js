0332746ae861c6fe0e2609fc4b315dde17f9311b
themoengame <themoengamecoc@gmail.com>
2026-05-08 17:46:00 +0700

    Refactor CONFIG object and add new properties

--- config.js
+++ config.js
@@ -1,6 +1,29 @@
-export const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwkC8rnCnUjwgWPPj7WE3p6-3Fl36bElA_Tymjp2yTyvg09JRGtP4OyAk86k1w71-YnjA/exec', // GANTI URL ANDA
-  STOK_MINIMAL: 5,
-  CURRENCY_LOCALE: 'id-ID',
-  CURRENCY_CODE: 'IDR'
+// Konfigurasi API Google Sheets
+const CONFIG = {
+    // Ganti dengan URL Web App Google Apps Script Anda
+    // Cara membuat: Deploy > New deployment > Web app > Execute as "Me" > Who has access "Anyone"
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwkC8rnCnUjwgWPPj7WE3p6-3Fl36bElA_Tymjp2yTyvg09JRGtP4OyAk86k1w71-YnjA/exec',
+    
+    // Nama sheet yang digunakan
+    SHEET_NAMES: {
+        MASTER_BARANG: 'MASTER BARANG',
+        PELANGGAN: 'PELANGGAN',
+        SUPPLIER: 'SUPPLIER',
+        TRANSAKSI_PENJUALAN: 'TRANSAKSI PENJUALAN',
+        PEMBELIAN_RESTOCK: 'PEMBELIAN / RESTOCK'
+    },
+    
+    // Konfigurasi aplikasi
+    APP_NAME: 'POS Toko Kelontong',
+    CURRENCY: 'Rp',
+    TAX_RATE: 0,
+    DISCOUNT_RATE: 0
 };
+
+// Global variables
+let cart = [];
+let products = [];
+let customers = [];
+let suppliers = [];
+let restocks = [];
+let transactions = [];
