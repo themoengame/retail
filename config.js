19741dd113840dbb692575c764dd42d92e7a5659
themoengame <themoengamecoc@gmail.com>
2026-05-08 17:54:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // Konfigurasi API Google Sheets
 const CONFIG = {
-    // Ganti dengan URL Web App Google Apps Script Anda
-    // Cara membuat: Deploy > New deployment > Web app > Execute as "Me" > Who has access "Anyone"
+    // Web App URL Anda
     WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwkC8rnCnUjwgWPPj7WE3p6-3Fl36bElA_Tymjp2yTyvg09JRGtP4OyAk86k1w71-YnjA/exec',
     
     // Nama sheet yang digunakan
@@ -10,20 +9,20 @@ const CONFIG = {
         PELANGGAN: 'PELANGGAN',
         SUPPLIER: 'SUPPLIER',
         TRANSAKSI_PENJUALAN: 'TRANSAKSI PENJUALAN',
-        PEMBELIAN_RESTOCK: 'PEMBELIAN / RESTOCK'
+        PEMBELIAN_RESTOCK: 'PEMBELIAN / RESTOCK',
+        PENGELUARAN_OPERASIONAL: 'PENGELUARAN OPERASIONAL'
     },
     
     // Konfigurasi aplikasi
     APP_NAME: 'POS Toko Kelontong',
-    CURRENCY: 'Rp',
-    TAX_RATE: 0,
-    DISCOUNT_RATE: 0
+    CURRENCY: 'Rp'
 };
 
-// Global variables
-let cart = [];
-let products = [];
-let customers = [];
-let suppliers = [];
-let restocks = [];
-let transactions = [];
+// Cache untuk data
+let dataCache = {
+    products: [],
+    customers: [],
+    suppliers: [],
+    transactions: [],
+    restocks: []
+};
